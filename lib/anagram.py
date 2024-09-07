c792c2a019f94073095ca0c7b6b7d1ab2edcd23d
Tracy Kemboi <tkemboi72@gmail.com>
2024-09-07 19:49:30 +0300

    update anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self,list_of_words):
+        new_list_of_anagrams=[]
+        for word in list_of_words:
+            if sorted(word.lower()) == sorted(self.word)and word.lower() !=self.word:
+                new_list_of_anagrams.append(word)
+                
+        return new_list_of_anagrams
+                
